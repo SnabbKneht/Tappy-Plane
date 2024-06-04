48b435de635ce6d0acee7bb612d97256895b3393
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2024-06-04 18:14:49 +0200

    Add plane color variants

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -1,7 +1,15 @@
 extends RigidBody2D
 
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+
 @export var force: float
+
+
+func _ready():
+	var color_variants = animated_sprite.sprite_frames.get_animation_names()
+	animated_sprite.play(Array(color_variants).pick_random())
 
 
 func _process(_delta):
