f400b2dec5684fe470b5ac23af46998bc34901f0
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2024-06-08 10:54:25 +0200

    Add plane crash

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
 
+signal crashed(position)
+
+
 @onready var animated_sprite = $AnimatedSprite2D
 
 
@@ -15,3 +18,8 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("Tap"):
 		apply_impulse(Vector2.UP * force)
+
+
+func _on_body_entered(_body):
+	crashed.emit(position)
+	queue_free()
